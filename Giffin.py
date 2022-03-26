d02bc8d4ec0c21147d26b42e6e24bbe4cfa15075
D4ve-R <david.rechkemmer@gmail.com>
2022-03-26 16:52:14 +0100

    drawing graphs with pyplot

--- Giffin.py
+++ Giffin.py
@@ -1,4 +1,6 @@
 from pulp import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 prob = LpProblem("giffin", LpMinimize)
 x_B = LpVariable("x_B", 0)
@@ -25,3 +27,14 @@ if(prob.status):
     print(20*"-")
     print("Objective value: ", pulp.value(prob.objective))
     print(20*"#")
+
+x= np.linspace(0,50)
+fig, axs = plt.subplots(2)
+fig.suptitle('Giffin Paradox')
+axs[0].plot(x, -x + minDist, color='green')
+axs[0].plot(x, -(priceBoat/priceTrain)*x+(maxMoney/priceTrain), color='blue')
+axs[0].grid()
+axs[1].plot(x, -x + minDist, color='green')
+axs[1].plot(x, -((priceBoat+1)/priceTrain)*x+(maxMoney/priceTrain), color='blue')
+axs[1].grid()
+plt.show()
